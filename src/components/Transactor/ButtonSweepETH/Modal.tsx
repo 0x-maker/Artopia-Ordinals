635d60321fea552a8bfdb6b9773f4e665b1837fa
SangNX <matterhorn@generative.xyz>
2023-03-20 17:49:55 +0700

    fix: toast error buy

--- src/components/Transactor/ButtonSweepETH/Modal.tsx
+++ src/components/Transactor/ButtonSweepETH/Modal.tsx
@@ -137,6 +137,11 @@ const ModalSweepBTC = React.memo(({ tokens, onHide, ...rest }: IProps) => {
 
   const removeUnBuyableToken = (data?: { [key: string]: string }) => {
     const orderIDs = Object.keys(data || {});
+    if (orderIDs.length === tokens.length) {
+      return toast.error(
+        'All your selected inscriptions not available for buy now.'
+      );
+    }
     for (const orderID of orderIDs) {
       removeSelectedOrder(orderID);
     }
