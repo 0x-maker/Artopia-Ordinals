5dd2a8dff94b92c6271b95ed826adb8847b168d9
Archaon <archaon@generative.xyz>
2023-03-21 17:06:50 +0700

    style: update ui ux

--- src/containers/GenerativeProjectDetail/MintLayout/index.tsx
+++ src/containers/GenerativeProjectDetail/MintLayout/index.tsx
@@ -1,11 +1,10 @@
 import CollectionList from '@components/Collection/List';
 import { TriggerLoad } from '@components/TriggerLoader';
 import { GenerativeProjectDetailContext } from '@contexts/generative-project-detail-context';
-import { useContext, useMemo } from 'react';
-import { Tab, Tabs } from 'react-bootstrap';
-import styles from '../styles.module.scss';
-import TokenTopFilter from '../TokenTopFilter';
 import cs from 'classnames';
+import { useContext, useMemo } from 'react';
+import TokenTopFilter from '../TokenTopFilter';
+import styles from '../styles.module.scss';
 
 const MintLayout = () => {
   const {
@@ -28,31 +27,31 @@ const MintLayout = () => {
   }, [projectInfo?.traitStat, marketplaceData?.listed]);
 
   return (
-    <Tabs className={styles.tabs} defaultActiveKey="outputs">
-      <Tab tabClassName={styles.tab} eventKey="outputs" title="Outputs">
-        {hasFilter && (
-          <div className={cs(styles.filterWrapper)} id="PROJECT_LIST">
-            <TokenTopFilter className={styles.filter_sort} />
-          </div>
-        )}
-        <div className={styles.tokenListWrapper} id="PROJECT_LIST">
-          <div className={styles.tokenList}>
-            <CollectionList
-              projectInfo={projectInfo}
-              listData={listItems}
-              isLoaded={isLoaded}
-              layout="mint"
-            />
-            <TriggerLoad
-              len={listItems?.length || 0}
-              total={total || 0}
-              isLoaded={isNextPageLoaded}
-              onEnter={handleFetchNextPage}
-            />
-          </div>
+    // <Tabs className={styles.tabs} defaultActiveKey="outputs">
+    <div>
+      {hasFilter && (
+        <div className={cs(styles.filterWrapper)} id="PROJECT_LIST">
+          <TokenTopFilter className={styles.filter_sort} />
         </div>
-      </Tab>
-    </Tabs>
+      )}
+      <div className={styles.tokenListWrapper} id="PROJECT_LIST">
+        <div className={styles.tokenList}>
+          <CollectionList
+            projectInfo={projectInfo}
+            listData={listItems}
+            isLoaded={isLoaded}
+            layout="mint"
+          />
+          <TriggerLoad
+            len={listItems?.length || 0}
+            total={total || 0}
+            isLoaded={isNextPageLoaded}
+            onEnter={handleFetchNextPage}
+          />
+        </div>
+      </div>
+    </div>
+    // </Tabs>
   );
 };
 
